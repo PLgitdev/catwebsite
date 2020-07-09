eee64113009b525c9151ac04a93c2f77850a0fdd
PLgitdev <plyssikatos@gmail.com>
2020-07-08 20:19:54 -0700

    added keys and clean up

--- src/Tabs.js
+++ src/Tabs.js
@@ -1,22 +1,17 @@
-import React from 'react';
-import Nav from 'react-bootstrap/Nav';
+import React from "react";
+import Nav from "react-bootstrap/Nav";
 
 const Tabs = () => {
-
-  const links = ["link1","link2","link3"]
+  const links = ["link1", "link2", "link3"];
 
   return (
     <Nav className="mr-auto">
-      { 
-        links.map( (link) => 
-          <Nav.Item>
-            <Nav.Link> 
-              { link }
-            </Nav.Link>
-          </Nav.Item>
-        )
-      }
+      {links.map((link) => (
+        <Nav.Item key={link.toString}>
+          <Nav.Link>{link}</Nav.Link>
+        </Nav.Item>
+      ))}
     </Nav>
-  ) 
-}
+  );
+};
 export default Tabs;
