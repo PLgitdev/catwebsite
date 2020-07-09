205babab44a3b76947d9cadf988ec7949a04e69a
PLgitdev <plyssikatos@gmail.com>
2020-07-08 20:20:51 -0700

    typo

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,18 +1,15 @@
-import React from 'react';
-import Navbar from 'react-bootstrap/Navbar';
-import Tabs from './Tabs.js';
-import ProfDropdown from './ProfDropdown.js';
-
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Tabs from "./Tabs.js";
+import ProfDropdown from "./ProfDropdown.js";
 
 const Navigation = () => {
-
-  return (    
-    <Navbar fill bg="light" > 
-      <Navbar.Brand> Cat website </Navbar.Brand> 
+  return (
+    <Navbar fill bg="light">
+      <Navbar.Brand> Cat website </Navbar.Brand>
       <Tabs />
       <ProfDropdown />
     </Navbar>
-  )
-}
+  );
+};
 export default Navigation;
-
