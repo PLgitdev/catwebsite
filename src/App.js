b0e59d386c178fc4264aa328ed2631e784f36033
PLgitdev <plyssikatos@gmail.com>
2020-07-08 20:22:28 -0700

    format

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import './App.css';
-import Homepage from './Homepage.js';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Signup from './Signup.js'
+import React from "react";
+import "./App.css";
+import Homepage from "./Homepage.js";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Signup from "./Signup.js";
 
 const App = () => {
   return (
     <Router>
       <Switch>
-        <Route path='/' exact={ true } component={ Homepage }/>
-        <Route path='/signup' exact={ true } component={ Signup }/>
+        <Route path="/" exact={true} component={Homepage} />
+        <Route path="/signup" exact={true} component={Signup} />
       </Switch>
     </Router>
-  )
-}
+  );
+};
 
 export default App;
--- src/GroupsList.js
+++ src/GroupsList.js
@@ -1,27 +1,21 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 const GroupsList = () => {
-  const [groups, setGroups] = useState([]) 
+  const [groups, setGroups] = useState([]);
   const [hasErrors, setErrors] = useState(false);
 
   useEffect(() => {
-    fetch('http://localhost:8080/api/groups', { mode: 'cors' })
-      .then(response => response.json())
-      .then(response => setGroups(response))
+    fetch("http://localhost:8080/api/groups", { mode: "cors" })
+      .then((response) => response.json())
+      .then((response) => setGroups(response))
       .catch(() => setErrors(true));
   }, []);
 
   return (
     <div>
-    {
-      !hasErrors ? groups.map(group =>
-        <div key={ group.id }>
-          { group.name }
-        </div>
-      )
-      : JSON.stringify(hasErrors)
-    }
+      {!hasErrors
+        ? groups.map((group) => <div key={group.id}>{group.name}</div>)
+        : JSON.stringify(hasErrors)}
     </div>
-  )
-}
+  );
+};
 export default GroupsList;
-
