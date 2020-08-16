a391b6d6742dc24012ab9360a37644e35b23e53e
PLgitdev <plyssikatos@gmail.com>
2020-08-16 07:14:40 -0600

    addded routing to sign in page

--- src/Tabs.js
+++ src/Tabs.js
@@ -1,17 +1,38 @@
 import React from "react";
 import Nav from "react-bootstrap/Nav";
+import { Link } from "react-router-dom";
+import { NavItem, NavLink } from "react-bootstrap";
 
 const Tabs = () => {
-  const links = ["link1", "link2", "link3"];
+  const links = [
+    {
+      link: "/signup",
+      name: "Sign up",
+    },
+    {
+      link: "/faq",
+      name: "Faq",
+    },
+    {
+      link: "/catinfo",
+      name: "Cat Information",
+    },
+  ];
 
   return (
     <Nav className="mr-auto">
-      {links.map((link) => (
-        <Nav.Item key={link.toString}>
-          <Nav.Link>{link}</Nav.Link>
-        </Nav.Item>
+      {links.map((key, index) => (
+        <Link key={index} to={`${key.link}`}>
+          <NavItem>
+            <NavLink>
+              {key.name}
+              {console.log(key)}
+            </NavLink>
+          </NavItem>
+        </Link>
       ))}
     </Nav>
   );
 };
+
 export default Tabs;
